22e95e20327613069bf3b5b8f88ee1854a9c2c02
Lucas Lima <lucasg17.lima@gmail.com>
2023-08-15 21:28:15 -0300

    teste db

--- src/database/mysql.py
+++ src/database/mysql.py
@@ -5,7 +5,7 @@ class MysqlDB:
         self.conn = None
         self.database = None
     
-    def connection(self, user:str = 'root', password:str = '', host:str = 'localhost', port:str = '3306', database:str = 'despesas'):
+    def connection(self, user:str = 'root', password:str = 'modelagem123!', host:str = '34.173.128.87', port:str = '3306', database:str = 'despesas'):
         self.conn = mysql.connector.connect(user=user, password=password, host=host, port=port, database=database)
     
     def close(self):
